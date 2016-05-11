726494253d1e94249896f785f5514d0c085719f4
Vladimir Konovalov <vlladimir.konovalov@gmail.com>
2016-05-12 00:39:14 +0300

    add sepset

---
+++ sepset/sepset/sepset.cpp
@@ -0,0 +1,76 @@
+#include "sepset.h"
+
+sepset::sepset(int s) {
+	size = s;
+	parent = new int[size];
+	rank = new int[size];
+	status = new int[size];
+	for (int i = 0; i < size; i++) {
+		parent[i] = i;
+		rank[i] = 0;
+	}
+}
+
+sepset::~sepset() {
+	delete[] parent;
+	delete[] rank;
+	delete[] status;
+}
+
+void sepset::check_parents(int** table, int n) {
+	for (int i = 0; i < n; i++) {
+		int x = table[i][0];
+		int y = table[i][1];
+		link(x, y);
+	}
+}
+
+int sepset::check_status() {
+	int count = 0;
+	for (int i = 0; i < size; i++) {
+		status[i] = parent[i];
+		if (status[i] == i) {
+			count++;
+		}
+	}
+	return count;
+}
+
+void sepset::link(int x, int y) {
+	int s1 = parent[x];
+	int s2 = parent[y];
+	int r1 = rank[s1];
+	int r2 = rank[s2];
+	if (r1<r2) {
+		parent[s1] = s2;
+	}
+	else {
+		parent[s2] = s1;
+		if (r1 == r2)
+			rank[s1] = rank[s2] + 1;
+	}
+}
+
+void sepset::separate_set(int** table, int n) {
+	check_parents(table, n);
+	int count_parents = check_status();
+	for (int j = 0; j < count_parents; j++) {
+		cout << "set [" << j << "] = {";
+		int cur_status = -1;
+		int i = 0;
+		for (int k = 0; k < size; k++) {
+			if (status[k] != -1) {
+				cur_status = status[k];
+				i = k;
+				break;
+			}
+		}
+		for (; i < size; i++) {
+			if (status[i] == cur_status) {
+				status[i] = -1;
+				cout << " " << i;
+			}
+		}
+		cout << " } " << endl;
+	}
+}
---
+++ sepset/sepset/source.cpp
@@ -0,0 +1,27 @@
+#include "sepset.h"
+
+void main() {
+	int rel = 0;
+	int size = 0;
+
+	cout << "Enter number of elements: "; cin >> size;
+
+	cout << "Enter the number of relations: "; cin >> rel;
+
+	int** relations;
+
+	relations = new int *[rel];
+	for (int i = 0; i < rel; i++) {
+		relations[i] = new int[2];
+	}
+
+	cout << "Enter the relations: " << endl;
+	for (int i = 0; i < rel; i++) {
+		cout << "Relation " << i << " 1: "; cin >> relations[i][0];
+		cout << "Relation " << i << " 2: "; cin >> relations[i][1];
+		cout << endl;
+	}
+
+	sepset ds(size);
+	ds.separate_set(relations, rel);
+}
